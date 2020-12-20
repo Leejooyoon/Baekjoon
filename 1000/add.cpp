bd45e717687964cbf3dbe6ec63d2b4703ae8c05a
Leejooyoon <ljy9795@naver.com>
2020-12-20 22:48:35 +0900

    main branch test

---
+++ 1000/add.cpp
@@ -0,0 +1,10 @@
+# include <iostream>
+
+using namespace std;
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int c =  a + b;
+	cout << c << endl;
+	// cin, cout 사용시에는 iostream 사용	
+}
---
+++ 10172/doggy.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+
+ 	printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+	// 따옴표를 출력하고 싶으면 앞에 \를 적어주면 됨 
+
+}
