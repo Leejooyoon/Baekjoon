0376ec9d52c702759d44986bea526656807fe099
Leejooyoon <ljy9795@naver.com>
2021-02-16 11:26:46 +0900

    2741

---
+++ lv3/2741-n찍기/2741.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+// https://www.acmicpc.net/problem/2741
+
+int main () {
+    int n;
+
+
+    cin >> n;
+
+   for(int i=1; i<n+1; i++){
+       cout << i << '\n';
+   }
+
+}
